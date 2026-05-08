d621f0e377c63d5a1340caaab154cac88028d14b
Fernando <fesenha@gmail.com>
2026-05-08 10:34:13 -0300

    algumas novas funcionalidades adicionadas

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -16,7 +16,7 @@ const longText = Cypress._.repeat('Lorem ipsum dolor sit amet, consectetur adipi
     cy.get('input[value=feedback]').check()
     cy.get('input[value=email]').check()
     cy.get('#open-text-area').type(longText, { delay: 0 })
-    cy.get('button[type=submit]').click()
+    cy.contains('button', 'Enviar').click()
     cy.get('.success').should('be.visible')
   })
     //cy.get('#file-upload').selectFile('cypress/fixtures/cachorro.jpg')
@@ -30,8 +30,8 @@ const longText = Cypress._.repeat('Lorem ipsum dolor sit amet, consectetur adipi
     cy.get('#product').select('YouTube')
     cy.get('input[value=email]').check()
     cy.get('#open-text-area').type('Teste de email inválido')
-    cy.get('button[type=submit]').click()
-	  cy.get('.error').should('be.visible')
+    cy.contains('button', 'Enviar').click()
+    cy.get('.error').should('be.visible')
   })
 
   it('Campo telefone continua vazio quando preenchido com letras', () => {
@@ -47,7 +47,7 @@ const longText = Cypress._.repeat('Lorem ipsum dolor sit amet, consectetur adipi
     cy.get('input[value=feedback]').check()
     cy.get('input[value=phone]').check()
     cy.get('#open-text-area').type(longText, { delay: 0 })
-    cy.get('button[type=submit]').click()
+    cy.contains('button', 'Enviar').click()
     cy.get('.error').should('be.visible')
   })
 
@@ -75,13 +75,80 @@ const longText = Cypress._.repeat('Lorem ipsum dolor sit amet, consectetur adipi
   })
 
   it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
-    cy.get('button[type=submit]').click()
+    cy.contains('button', 'Enviar').click()
     cy.get('.error').should('be.visible')
   })
 
-  it.only('envia o formulário com sucesso usando um comando customizado', () => {
+  it('envia o formulário com sucesso usando um comando customizado', () => {
+    const data = {
+      firstName: 'Maria',
+      lastName: 'Silva',
+      email: 'maria.silva@example.com',
+      text: 'Teste'
+    }
+
+    cy.fillMandatoryFieldsAndSubmit(data)
+    cy.get('.success').should('be.visible')
+
     cy.fillMandatoryFieldsAndSubmit()
     cy.get('.success').should('be.visible')
   })
+
+it('Testa os campos obrigatórios e envia o formulário com o cy.contains', () => {
+    cy.get('#firstName').type('João')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('joao.silva@example.com')
+    cy.get('#product').select('YouTube')
+    cy.get('input[value=feedback]').check()
+    cy.get('input[value=email]').check()
+    cy.get('#open-text-area').type(longText, { delay: 0 })
+    cy.contains('button', 'Enviar').click()
+    cy.get('.success').should('be.visible')
+  })
+
+it('seleciona um produto (YouTube) por seu texto', () => {
+	cy.get('#product').select('YouTube')
+	cy.get('#product').should('have.value', 'youtube')
+})
+
+it('seleciona um produto (Mentoria) por seu valor', () => {
+	cy.get('#product').select('mentoria')
+	cy.get('#product').should('have.value', 'mentoria')
+})
+
+it('seleciona um produto (Blog) por seu índice', () => {
+	cy.get('#product').select(1)
+	cy.get('#product').should('have.value', 'blog')
+})
+
+//Conhecendo a funcionalidade should(be.checked)
+it('marca o tipo de atendimento "Feedback"', () => {
+	cy.get('input[value=feedback]').check()
+	cy.get('input[value=feedback]').should('be.checked')
+})
+
+//Conhecendo a funcionalidade cy.each e cy.wrap
+it('marca cada tipo de atendimento com cy.each e cy.wrap', () => {
+	cy.get('input[type=radio]')
+    .each(typeOfService => {
+      cy.wrap(typeOfService)
+        .check()
+        .should('be.checked')
+    })
+})
+
+//Descobrindo a funcionalidade first(), last() e uncheck()
+it('marca ambos checkboxes, depois desmarca o último', () => {
+	cy.get('input[type=checkbox]')
+    .check()
+		.should('be.checked')
+	cy.get('input[type=checkbox]')
+		.last()
+		.uncheck()
+		.should('not.be.checked')
+  cy.get('input[type=checkbox]')
+    .first()
+    .should('be.checked')
+})
 
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,12 @@
-Cypress.Commands.add('fillMandatoryFieldsAndSubmit', () => {
-    cy.get('#firstName').type('João')
-    cy.get('#lastName').type('Silva')
-    cy.get('#email').type('joao.silva@example.com')
-    cy.get('#open-text-area').type('Teste')
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (data = {
+    firstName: 'João',
+    lastName: 'Silva',
+    email: 'joao.silva@example.com',
+    text: 'Teste'
+  }) => {
+    cy.get('#firstName').type(data.firstName)
+    cy.get('#lastName').type(data.lastName)
+    cy.get('#email').type(data.email)
+    cy.get('#open-text-area').type(data.text)
     cy.get('button[type=submit]').click()
   })
